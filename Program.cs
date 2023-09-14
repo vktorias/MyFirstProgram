f0847fe238941dbe3a72006373ae5d64a18632dc
viktoria wallström <viktoria-wallstrom@hotmail.com>
2023-09-14 08:45:28 +0200

    Ändringar.

--- Program.cs
+++ Program.cs
@@ -5,28 +5,38 @@
         //Viktoria Wallström .NET23
         static void Main(string[] args)
         {
-            
+            //Declare an integer variable. 
             int Number = 15;
-            
+
+            // Check if the value of "Number" is greater than 10.
             if (Number > 10)
             { 
+                //If "Number" is greater than 10, print this message. 
                 Console.WriteLine("Talet är stort!");
             }
+            //Check if "Number" is less than 10.
             else if (Number < 10)
             {
+                //If "Number" is less than 10, print this message. 
                 Console.WriteLine("Talet är lågt.");
             }
            
+            //Ask for user input
             Console.WriteLine("Vad heter du?");
 
-            string name = Console.ReadLine();
+            //Read the user input and store it in the "Name" variable.
+            string userName = Console.ReadLine();
 
-            Console.WriteLine("Hej " + name + ("!"));
+            //Print "Hej" and the user name. 
+            Console.WriteLine("Hej " + userName + ("!"));
 
-            
-            for (int i = 0; i<16; i++)
+            //Use a for loop to iterate from 0 up to 15.
+            for (int i = 0; i <= Number; i++)
             {
+            
+                //Print the current value of "i".
                 Console.WriteLine(i);
+            
             }
 
         }
